b5fc724ede969a762f12760a1374ce528c70b809
Sabah-Zerin <sabahzerin1504@gmail.com>
2024-12-21 20:05:12 +0600

    new features

---
+++ src/views/AboutUs.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+export default function AboutUs() {
+  return (
+    <section className="text-center mt-4">
+      <h2>About Us</h2>
+      <div className="mt-4">
+        <h3>Our Mission</h3>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas luc lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas luc.
+        </p>
+      </div>
+      <div className="mt-4">
+        <h3>Our History</h3>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas luc lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas luc.
+        </p>
+      </div>
+      <div className="mt-4">
+       
+      </div>
+    </section>
+  );
+}
